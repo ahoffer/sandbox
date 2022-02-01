b33081309bc4d7561021d052bc8dcd2607fd5ded
aaron.hoffer <aaron.hoffer@connexta.com>
2022-02-01 16:40:38 -0700

    Tweaks

--- py/client.py
+++ py/client.py
@@ -1,18 +1,20 @@
 #! /usr/bin/python3
-from proton import Message
+import proton
+from proton import Message, Connection
 from proton.handlers import MessagingHandler
 from proton.reactor import Container
 import settings
 import argparse
+
 
 parser = argparse.ArgumentParser(description='AMQP client for WPS testing.')
 parser.add_argument('roles', help='The name of one or more brokers: live, backup, other')
 parser.add_argument('hosts', nargs='+', help='sum the integers (default: find the max)')
 args = parser.parse_args()
 
-class Recv(MessagingHandler):
+class Client(MessagingHandler):
     def __init__(self):
-        super(Recv, self).__init__()
+        super(Client, self).__init__()
         self.container = None
         self.connection = None
         self.brokers = [settings.all[e] for e in args.hosts]
@@ -21,7 +23,7 @@ class Recv(MessagingHandler):
     def on_start(self, event):
         print(self.brokers)
         self.container = event.container
-        self.connection = self.container.connect(urls=self.brokers, user='admin', password='admin')
+        self.connection = self.container.connect(urls=self.brokers, user='admin', password='admin', reconnect=True)
         if args.roles == 'consumer':
             event.container.create_receiver(self.connection, source='wps.v1.result')
             print('Waiting...')
@@ -49,6 +51,6 @@ class Recv(MessagingHandler):
         self.send()
 
 try:
-    Container(Recv()).run()
+    Container(Client()).run()
 except KeyboardInterrupt:
     pass
