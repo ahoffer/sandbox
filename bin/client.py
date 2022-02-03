9ce12fd35757b3474356a1942739ed843ad0523e
aaron.hoffer <aaron.hoffer@connexta.com>
2022-02-03 16:31:14 -0700

    Losts of changes to client, a few changes to playbook. Each node should now have a "check" command to show it's status and its place in the cluster.

--- bin/client.py
+++ bin/client.py
@@ -6,9 +6,10 @@ from proton.reactor import Container
 import settings
 import argparse
 
-parser = argparse.ArgumentParser(description='AMQP client for WPS testing.')
+parser = argparse.ArgumentParser(description='AMQP client for testing.')
 parser.add_argument('role', help='producer or consumer')
-parser.add_argument('hosts', nargs='+', help='The name of one or more brokers: live, backup, other')
+parser.add_argument('hosts', nargs='+', help='The name of one or more brokers', choices=['live', 'backup', 'other'])
+parser.add_argument('-a', help='Address. For example, wps.v1.result', required=True, nargs=1)
 args = parser.parse_args()
 
 # What does an event know about?
@@ -21,10 +22,11 @@ args = parser.parse_args()
 # event.delivery
 # event.message
 
-
 class Client(MessagingHandler):
     def __init__(self):
         super(Client, self).__init__()
+        self.address = None
+        self.receiver = None
         self.container = None
         self.connection = None
         self.brokers = [settings.all[e] for e in args.hosts]
@@ -32,16 +34,20 @@ class Client(MessagingHandler):
 
     def on_start(self, event):
         print(self.brokers)
+        self.address = args.a[0]
+        # self.address = "wps.v1.execute.req"
         self.container = event.container
         self.container.container_id = args.role + '-' + '_'.join(args.hosts)
         self.connection = self.container.connect(urls=self.brokers, user='admin', password='admin')
+        result = 'sensor.replay'
         if args.role == 'consumer':
-            receiver = event.container.create_receiver(self.connection, source='wps.v1.result')
-            # DurableSubscription().apply(receiver)
-            print('Waiting...')
+            self.receiver = event.container.create_receiver(self.connection, source=self.address)
+            DurableSubscription().apply(self.receiver)
+            print("Consuming from address " + self.address)
         elif args.role == 'producer':
-            self.sender = self.container.create_sender(self.connection, 'wps.v1.result')
-            # AtLeastOnce().apply(self.sender)
+            self.sender = self.container.create_sender(self.connection, target=self.address)
+            AtLeastOnce().apply(self.sender)
+            print("Sending to address " + self.address)
             self.container.schedule(2, self)
         else:
             print('Did not understand role {role}'.format(role=args.role))
@@ -49,6 +55,8 @@ class Client(MessagingHandler):
 
     def on_message(self, event):
         print('Receiving\t', event.message)
+        # This next part was in the sample code.
+        # I don't know enough about connections and closing, and so I commented it out.
         # if event.receiver.queued == 0 and event.receiver.drained:
         #     print('Nothing queued')
         #     event.connection.close()
@@ -59,6 +67,7 @@ class Client(MessagingHandler):
         self.count += 1
         print('Sending {count}\t{body}'.format(count=self.count, body=msg.body))
         self.container.schedule(2, self)
+
     def on_timer_task(self, event):
         self.send()
 
@@ -98,6 +107,8 @@ class Client(MessagingHandler):
 
     def on_connection_closed(self, event):
         print("connection closed")
+
+
 try:
     Container(Client()).run()
 except KeyboardInterrupt:
